a0314162eda3175d4f24f89f694ede8aadd6e73e
Kellyson Dias <77993169+kellysondias@users.noreply.github.com>
2022-10-27 14:19:40 -0300

    Update schema.jsx

--- src/schema.jsx
+++ src/schema.jsx
@@ -1,5 +1,10 @@
 import * as Yup from 'yup'
 
+// Para instalar o Yup use "npm add yup" ou "yarn add yup"
+
+/* O schema define como a sua estrutura de dados deve ser respeitada. 
+Se o objeto precisa de uma atributo name como string, por exemplo. Com o schema você consegue fazer uma validação muito melhor. */}
+
 export default Yup.object().shape({
     name: Yup.string().min(2).required(),
     email: Yup.string().email().required()
